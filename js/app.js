fcdfde5cc0e62367f61abd68afb0d70a07848da0
Mercy Griffin <teachmercy@gmail.com>
2020-08-12 10:52:43 -0500

    edit HTML & app.js

--- js/app.js
+++ js/app.js
@@ -21,88 +21,21 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("COVID-19_Data_by_ZIP_Code.csv").then(function(hairData){
-
-    // Step 1: Parse Data/Cast as numbers
-    // ==============================
-    COVID-19_Data_by_ZIP_Code.forEach(function(data) {
-      data.ZipCode = +data.ZipCode;
-      data.COVID19LCases = +data.COVID19LCases;
-      data.CrudeRatePer100,000 = +data.COVID19LCases;
-    });
-
-    // Step 2: Create scale functions
-    // ==============================
-    var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(hairData, d => d.hair_length)])
-      .range([0, width]);
-
-    var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(hairData, d => d.num_hits)])
-      .range([height, 0]);
-
-    // Step 3: Create axis functions
-    // ==============================
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-    // Step 4: Append Axes to the chart
-    // ==============================
-    chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
-
-    chartGroup.append("g")
-      .call(leftAxis);
-
-    // Step 5: Create Circles
-    // ==============================
-    var circlesGroup = chartGroup.selectAll("circle")
-    .data(hairData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.hair_length))
-    .attr("cy", d => yLinearScale(d.num_hits))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
-
-    // Step 6: Initialize tool tip
-    // ==============================
-    var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
+Promise.all([
+    d3.csv("COVID-19_Data_by_ZIP_Code.csv"),
+    d3.csv("KCPD_Crime_Data_2020.csv"),
+    ]).then(function(KCPD_Crime_Data_2020)
+    {
+      var p1 = new Promise((resolve, reject) => { 
+        setTimeout(() => resolve('p1_delayed_resolution'), 1000); 
+      }); 
+      var p2 = new Promise((resolve, reject) => {
+        reject(new Error('p2_immediate_rejection'));
       });
-
-    // Step 7: Create tooltip in the chart
-    // ==============================
-    chartGroup.call(toolTip);
-
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
-    circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
-    })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
-
-    // Create axes labels
-    chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
-
-    chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
-  }).catch(function(error) {
-    console.log(error);
-  });
+      Promise.all([
+        p1.catch(error => { return error }),
+        p2.catch(error => { return error }),
+      ]).then(values => { 
+        console.log(values[0]) // "p1_delayed_resolution"
+        console.error(values[1]) // "Error: p2_immediate_rejection"
+      })
